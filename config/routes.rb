cbfead6f84fec53f29c6a40011e47b7327b17ca3
ninunu7 <pibf51@gmail.com>
2021-05-09 09:15:12 +0000

    signin

--- config/routes.rb
+++ config/routes.rb
@@ -3,34 +3,20 @@ Rails.application.routes.draw do
     sessions: 'admins/sessions'
   }
 
-
   devise_for :customers, controllers: {
-    registrations: 'customers/registrations',
     sessions: 'customers/sessions'
   }
 
-  namespace :customer do
-    get 'blogs/new'
-    get 'blogs/index'
-    get 'blogs/show'
-  end
-  namespace :customer do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/quit'
-  end
-  namespace :customer do
-    get 'events/new'
-    get 'events/edit'
-  end
-  namespace :customer do
-    get 'searches/top'
-    get 'searches/index'
-  end
-  namespace :customer do
-    root to: 'homes#top'
+  scope module: :customer do
+
     get 'homes/top'
     get 'homes/about'
+    resources :blogs
+    resources :customers
+    get 'customers/quit'
+    resources :events
+    resources :searches
+    root to: 'homes#top'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

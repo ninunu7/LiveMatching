b551cd59f0ff6888762e8d74d1438f71644fc2af
ninunu7 <pibf51@gmail.com>
2021-05-11 15:21:47 +0000

    ブログ

--- app/controllers/customer/blogs_controller.rb
+++ app/controllers/customer/blogs_controller.rb
@@ -3,8 +3,29 @@ class Customer::BlogsController < ApplicationController
   end
 
   def index
+    @blog = Blog.new
+    @blogs = Blog.all
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.save
+    redirect_to blogs_path, notice: "投稿しました"
   end
 
   def show
+    @blog = Blog.find(params[:id])
+    @blog_comment = BlogComment.new
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:blog_title, :blog_text)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module LiveMatching
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'#投稿日時を東京の時間に変更
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +18,4 @@ module LiveMatching
     # the framework and any gems in your application.
   end
 end
+

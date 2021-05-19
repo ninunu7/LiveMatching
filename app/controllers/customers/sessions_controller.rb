c430701205df46b61587e0e32d089f255769825f
ninunu7 <pibf51@gmail.com>
2021-05-19 15:30:17 +0000

    view修正

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -11,7 +11,6 @@ class Customers::SessionsController < Devise::SessionsController
   # # POST /resource/sign_in
   #  # def create
   #  #   super
-  #  #   flash[:alert] = "退会済みです。"
   #  # end
 
   # # DELETE /resource/sign_out
@@ -21,8 +20,6 @@ class Customers::SessionsController < Devise::SessionsController
 
   #   protected
   #   def configure_sign_in_params
-  #    # めーるアドレスが無い場合
-
   #    if params[:customer][:email].blank? || params[:customer][:password].blank?
   #     flash[:error] = "ログインに失敗しました。"
   #     redirect_to new_customer_session_path

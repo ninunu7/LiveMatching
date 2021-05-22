50841ad52184a3ece73b76201ef8b3a77332a498
ninunu7 <pibf51@gmail.com>
2021-05-22 14:27:18 +0000

    非同期

--- app/controllers/customer/comments_controller.rb
+++ app/controllers/customer/comments_controller.rb
@@ -1,7 +1,6 @@
 class Customer::CommentsController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    #comment = Comment.create(comment_params)
     @comment = current_customer.comments.new(comment_params)
     @comment.event_id = @event.id
     if @comment.save
@@ -13,10 +12,7 @@ class Customer::CommentsController < ApplicationController
   def destroy
     @event = Event.find(params[:event_id])
     @comment = Comment.find_by(id: params[:id], event_id: params[:event_id]).destroy
-    #redirect_to event_path(params[:event_id])
   end
-    #@blog_comment = BlogComment.find_by(id: params[:id], blog_id: params[:blog_id])
-
 
   private
   def comment_params
--- app/controllers/customer/relationships_controller.rb
+++ app/controllers/customer/relationships_controller.rb
@@ -1,13 +1,13 @@
 class Customer::RelationshipsController < ApplicationController
 
   def create
+    @customer = Customer.find(params[:customer_id])
     current_customer.follow(params[:customer_id])
-    redirect_to request.referer
   end
 
   def destroy
+    @customer = Customer.find(params[:customer_id])
     current_customer.unfollow(params[:customer_id])
-    redirect_to request.referer
   end
 
   def followings

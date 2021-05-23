e8a2065256d6ffe388b2432c8f6fa3343a4c17ad
ninunu7 <pibf51@gmail.com>
2021-05-23 11:41:16 +0000

    top修正

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,5 +1,7 @@
 class Customer::HomesController < ApplicationController
   def top
+    @customers = current_customer.followings
+    @blogs = Blog.where(customer_id: [current_customer.id, *current_customer.following_ids])
   end
 
   def about

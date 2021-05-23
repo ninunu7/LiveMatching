5c0c7b6431a377098bc91948f3c9bca48ddb5f77
ninunu7 <pibf51@gmail.com>
2021-05-23 12:32:29 +0000

    top

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,7 +1,8 @@
 class Customer::HomesController < ApplicationController
   def top
-    @customers = current_customer.followings
-    @blogs = Blog.where(customer_id: [current_customer.id, *current_customer.following_ids])
+    if current_customer != nil
+      @blogs = Blog.where(customer_id: [current_customer.id, *current_customer.following_ids]).order(created_at: :desc).limit(10)
+    end
   end
 
   def about

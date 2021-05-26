0a51a03bf11910c782c45995ac850a6d1540d9f6
ninunu7 <pibf51@gmail.com>
2021-05-26 11:57:45 +0000

    layout

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,8 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20}
-  validates :email, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, uniqueness: true , format: { with: VALID_EMAIL_REGEX }}
   validates :password, presence: true, on: :create
 
   attachment :profile_image

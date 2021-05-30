6f297743e4f683886dbc42203ec418d726d8f8cd
ninunu7 <pibf51@gmail.com>
2021-05-30 07:51:26 +0000

    guestfix

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -6,7 +6,7 @@ class Customers::SessionsController < Devise::SessionsController
   def guest_sign_in
     customer = Customer.guest
     sign_in customer
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to root_path
   end
 
   # # GET /resource/sign_in

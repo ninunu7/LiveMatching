2de8f056068e736568d740578dc9177c17e3b2b7
ninunu7 <pibf51@gmail.com>
2021-07-09 14:04:34 +0000

    [add]rspec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,21 +1,32 @@
 require 'rails_helper'
 
   RSpec.describe Blog, "Blogモデルのテスト", type: :model do
-    describe '保存テスト' do
-      it "有効な投稿内容の場合は保存されるか" do
-        expect(FactoryBot.build(:blog)).to be_valid
-	    end
-	  end
-	  context "空欄でないか" do
+    describe 'バリデーションのテスト' do
+      subject { blog.valid? }
+
+      let(:customer) { create(:customer) }
+      let!(:blog) { build(:blog, customer_id: customer.id) }
+  #   describe '保存テスト' do
+  #     it "有効な投稿内容の場合は保存されるか" do
+  #       expect(FactoryBot.build(:blog)).to be_valid
+	 #   end
+	 # end
+	  context "titleカラム" do
       it "titleが空欄でないか" do
-        blog = Blog.new(title: '', body:'hoge')
-	      expect(blog).to be_invalid
-	      expect(blog.errors[:blog_title]).to include("can't be blank")
+      #   blog = Blog.new(title: '', body:'hoge')
+	     # expect(blog).to be_invalid
+	     # expect(blog.errors[:blog_title]).to include("を入力してください")
+	      blog.title = ''
+        is_expected.to eq false
       end
+    end
+
+    context "textカラム" do
       it "textが空欄でないか" do
         blog = Blog.new(title: 'hoge', body:'')
         expect(blog).to be_invalid
-	      expect(blog.errors[:blog_text]).to include("can't be blank")
+	      expect(blog.errors[:blog_text]).to include("を入力してください")
       end
     end
   end
+end

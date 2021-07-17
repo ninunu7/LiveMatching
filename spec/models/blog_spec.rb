977569b939cc9b5457179fc8aeb7784cc7a78ad8
ninunu7 <pibf51@gmail.com>
2021-07-17 09:18:27 +0000

    [add]rspec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -7,18 +7,14 @@ require 'rails_helper'
       let(:customer) { create(:customer) }
       let!(:blog) { build(:blog, customer_id: customer.id) }
 
-
-  #   describe '保存テスト' do
-  #     it '有効な投稿内容の場合は保存されるか' do
-  #       expect(FactoryBot.build(:blog)).to be_valid
-	 #   end
-	 # end
+    describe '保存テスト' do
+      it '有効な投稿内容の場合は保存されるか' do
+        expect(FactoryBot.build(:blog)).to be_valid
+	    end
+	  end
 
 	  context 'titleカラム' do
       it 'titleが空欄でないか' do
-      #   blog = Blog.new(title: '', body:'hoge')
-	     # expect(blog).to be_invalid
-	     # expect(blog.errors[:blog_title]).to include("を入力してください")
 	      blog.blog_title = ''
         is_expected.to eq false
       end
@@ -26,11 +22,16 @@ require 'rails_helper'
 
     context 'textカラム' do
       it 'textが空欄でないか' do
-      #   blog = Blog.new(title: 'hoge', body:'')
-      #   expect(blog).to be_invalid
-	     # expect(blog.errors[:blog_text]).to include("を入力してください")
 	      blog.blog_text = ''
         is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Customerモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Blog.reflect_on_association(:customer).macro).to eq :belongs_to
       end
     end
   end
